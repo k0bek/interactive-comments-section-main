d4405e2c3f197601e7ca8babf669983d0f23deb1
Kuba <kubakiebala@gmail.com>
2022-11-30 23:34:35 +0100

    add createComment function and fetchJSON improved

--- js/main.js
+++ js/main.js
@@ -1,21 +1,71 @@
+const cardsSection = document.querySelector(".cards");
+
 const fetchJSON = async function () {
 	const JSON_URL = "./data.json";
 
-	const promise = new Promise((resolve, reject) => {
-		const fetchedJSON = fetch(JSON_URL)
-			.then((response) => {
-				if (response.status >= 200 && response.status < 300) {
-					return resolve(response.json());
-				} else {
-					return response.json().then((error) => {
-						reject(console.error("we cannot connect now. try later"));
-					});
-				}
-			})
-			.catch((error) => {
-				console.error("we cannot connect now. try later");
-			});
-	});
-
-	return promise;
+	return fetch(JSON_URL)
+		.then((response) => {
+			if (response.status >= 200 && response.status < 300) {
+				return response.json();
+			} else {
+				return response.json().then((error) => {
+					throw new Error("We cannot connect now. Try later");
+				});
+			}
+		})
+		.catch((error) => {
+			throw new Error("We cannot connect now. Try later");
+		});
 };
+
+async function getResponse() {
+	const fetchedJSON = await fetchJSON();
+	const { currentUser, comments } = fetchedJSON;
+
+	return currentUser, comments;
+}
+
+function createSingleComment(params) {}
+
+async function createComments() {
+	const commentsInformations = await getResponse();
+
+	for (const commentInfo of commentsInformations) {
+		const comment = document.createElement("div");
+		comment.classList.add("card");
+		cardsSection.append(comment);
+		comment.innerHTML = `
+		 <div class="card">
+
+		<div class="card-top">
+		  <div class="card-top__img">
+			<img src="./images/avatars/image-amyrobson.png" alt="">
+		  </div>
+		  <p class="card-top__username">amyrobson</p>
+		  <p class="card-top__time">1 month ago</p>
+		</div>
+
+		<p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem error atque
+		  voluptates at esse. Excepturi suscipit beatae fugit velit nesciunt ea earum, repellendus dolor aperiam.
+		</p>
+
+		<div class="card-bottom">
+		  <div class="card-bottom__votes">
+			<button class="card-bottom__votes--plus"><img src="./images/icon-plus.svg" alt=""></button>
+			<p class="card-bottom__votes-amount">12</p>
+			<button class="card-bottom__votes--minus"><img src="./images/icon-minus.svg" alt=""></button>
+		  </div>
+
+		  <button class="card-bottom__reply">
+			<span class="card-bottom__reply-icon"><img src="./images/icon-reply.svg" alt=""></span>
+			<p class="card-bottom__reply-text">Reply</p>
+		  </button>
+		</div>
+
+	  </div>
+
+		`;
+	}
+}
+
+createComments();
