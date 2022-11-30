2cf883497e3800b037877bf11a63f4310b5ff364
Kuba <kubakiebala@gmail.com>
2022-11-30 19:49:40 +0100

    fetchJSON function created

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-const JSON_URL = "./data.json";
+const fetchJSON = async function () {
+	const JSON_URL = "./data.json";
 
-const xhr = new XMLHttpRequest();
+	const promise = new Promise((resolve, reject) => {
+		const fetchedJSON = fetch(JSON_URL)
+			.then((response) => {
+				if (response.status >= 200 && response.status < 300) {
+					return resolve(response.json());
+				} else {
+					return response.json().then((error) => {
+						reject(console.error("we cannot connect now. try later"));
+					});
+				}
+			})
+			.catch((error) => {
+				console.error("we cannot connect now. try later");
+			});
+	});
 
-xhr.open("GET", JSON_URL);
-
-xhr.onload = function () {
-	console.log(JSON.parse(xhr.response));
+	return promise;
 };
-
-xhr.send();
