4d22252a4bec8defe569147209f6b6b5fe9c8942
Kuba <kubakiebala@gmail.com>
2022-12-01 19:47:07 +0100

    amendments with cards

--- js/main.js
+++ js/main.js
@@ -25,47 +25,54 @@ async function getResponse() {
 	return currentUser, comments;
 }
 
-function createSingleComment(params) {}
+// async function initializeComments() {
+// 	const commentsInformations = await getResponse();
 
-async function createComments() {
-	const commentsInformations = await getResponse();
+// 	for (const commentInfo of commentsInformations) {
+// 		const comment = document.createElement("div");
+// 		comment.classList.add("card");
+// 		cardsSection.append(comment);
+// 		comment.innerHTML = `
+// 		<div class="card__votes desktop">
+// 		<button class="card__votes--plus"><img src="./images/icon-plus.svg" alt=""></button>
+// 		<p class="card__votes-amount">12</p>
+// 		<button class="card__votes--minus"><img src="./images/icon-minus.svg" alt=""></button>
+// 	  </div>
 
-	for (const commentInfo of commentsInformations) {
-		const comment = document.createElement("div");
-		comment.classList.add("card");
-		cardsSection.append(comment);
-		comment.innerHTML = `
-		 <div class="card">
+// 	  <div class="card-content">
+// 		<div class="card-top">
+// 		  <div class="card-top__img">
+// 			<img src="./images/avatars/image-amyrobson.png" alt="">
+// 		  </div>
+// 		  <p class="card-top__username">amyrobson</p>
+// 		  <p class="card-top__time">1 month ago</p>
+// 		</div>
 
-		<div class="card-top">
-		  <div class="card-top__img">
-			<img src="./images/avatars/image-amyrobson.png" alt="">
-		  </div>
-		  <p class="card-top__username">amyrobson</p>
-		  <p class="card-top__time">1 month ago</p>
-		</div>
+// 		<p class="card-text">${commentInfo.content}
+// 		</p>
 
-		<p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem error atque
-		  voluptates at esse. Excepturi suscipit beatae fugit velit nesciunt ea earum, repellendus dolor aperiam.
-		</p>
+// 		<div class="card-bottom">
+// 		  <div class="card__votes mobile">
+// 			<button class="card__votes--plus"><img src="./images/icon-plus.svg" alt=""></button>
+// 			<p class="card__votes-amount">12</p>
+// 			<button class="card__votes--minus"><img src="./images/icon-minus.svg" alt=""></button>
+// 		  </div>
 
-		<div class="card-bottom">
-		  <div class="card-bottom__votes">
-			<button class="card-bottom__votes--plus"><img src="./images/icon-plus.svg" alt=""></button>
-			<p class="card-bottom__votes-amount">12</p>
-			<button class="card-bottom__votes--minus"><img src="./images/icon-minus.svg" alt=""></button>
-		  </div>
+// 		  <button class="card-bottom__reply">
+// 			<span class="card-bottom__reply-icon"><img src="./images/icon-reply.svg" alt=""></span>
+// 			<p class="card-bottom__reply-text">Reply</p>
+// 		  </button>
+// 		</div>
+// 	  </div>
 
-		  <button class="card-bottom__reply">
-			<span class="card-bottom__reply-icon"><img src="./images/icon-reply.svg" alt=""></span>
-			<p class="card-bottom__reply-text">Reply</p>
-		  </button>
-		</div>
+// 		`;
 
-	  </div>
+// 		const commentInfoArr = commentInfo.replies;
 
-		`;
-	}
-}
+// 		if (commentInfoArr.length !== 0) {
+// 			console.log(commentInfoArr);
+// 		}
+// 	}
+// }
 
-createComments();
+initializeComments();
